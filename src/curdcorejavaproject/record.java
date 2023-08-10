9b3d3226b8431b182db69fc9774d0f83f39c2675
Mayur <mayurtitirmare261@gmail.com>
2023-08-10 01:30:46 -0700

    Just i started to build project

---
+++ src/curdcorejavaproject/record.java
@@ -0,0 +1,12 @@
+package curdcorejavaproject;
+
+public class record {
+
+	String name;
+	int idnumber;
+	double mobile;
+	
+	public record(){}
+	
+	public record(String name, int idnumber,int mobile);
+}
